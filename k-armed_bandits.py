761b3439f2c8a24ae2eb390c9ff1461d79db4970
Aravinth Kulanthaivelu <aravinth@Aravinths-MacBook-Pro.local>
2024-08-07 11:18:05 +0900

    feat: parameterised initial means of rewards distribution

--- k-armed_bandits.py
+++ k-armed_bandits.py
@@ -9,27 +9,35 @@ class RewardDistribution:
     def __init__(
             self, 
             k, 
-            sampling_variance, 
+            sampling_variance,
+            init_mean_fn, 
             is_nonstationary=None, 
-            dist_mean_drift_fn=None,
-            **kwargs,
+            mean_drift_fn=None,
         ):
         self.k = k
-        self.initial_means = np.random.normal(size=k)
+        self._init_mean(init_mean_fn)
         self.means = np.copy(self.initial_means)
         self.variance = sampling_variance
         if is_nonstationary:
-            self.update_fn = dist_mean_drift_fn
+            self.update_fn = mean_drift_fn
             self.random_state = np.random.RandomState(seed=42)
 
-    def generate_rewards(self):
-        return np.random.normal(self.means, np.sqrt(self.variance))
+    def _init_mean(self, init_mean_fn):
+        if init_mean_fn=="equal":
+            self.initial_means = np.ones(size=self.k)*np.random.normal()
+        elif init_mean_fn=="standard_normal":
+            self.initial_means = np.random.normal(size=self.k)
+        else:
+            raise NotImplementedError
 
     def update_means(self, **kwargs):
         if self.update_fn=="standard_normal_increments":
             self.means += self.random_state.normal(size=self.k, **kwargs)
         else:
-            raise NotImplementedError   
+            raise NotImplementedError  
+
+    def generate_rewards(self):
+        return np.random.normal(self.means, np.sqrt(self.variance)) 
 
 
 class EpsilonGreedyPolicy:
@@ -60,13 +68,13 @@ class EpsilonGreedyPolicy:
     def update_action_value(self, action, reward, **kwargs):
         self.N[action] += 1
         if self.update_fn=="sample_average":
-            self.Q[action] += online_mean_update(
+            self.Q[action] = online_mean_update(
                 old_estimate=self.Q[action],
                 target=reward,
                 step_size=1.0/self.N[action],
             )
         elif self.update_fn=="constant_step_size":
-            self.Q[action] += online_mean_update(
+            self.Q[action] = online_mean_update(
                 old_estimate=self.Q[action],
                 target=reward,
                 **kwargs
@@ -82,12 +90,12 @@ class Metrics:
         self.run = 0
     
     def update(self, rewards, optimal_actions):
-        self.rewards += online_mean_update(
+        self.rewards = online_mean_update(
             old_estimate=self.rewards,
             target=rewards,
             step_size=1.0/(self.run + 1)
         )
-        self.optimal_actions += online_mean_update(
+        self.optimal_actions = online_mean_update(
             old_estimate=self.optimal_actions,
             target=optimal_actions,
             step_size=1.0/(self.run + 1)
@@ -102,7 +110,7 @@ def read_config(file_path):
 
 
 def online_mean_update(old_estimate, target, step_size):
-    return (target - old_estimate) * step_size
+    return old_estimate + (target - old_estimate) * step_size
 
 
 def run_bandits(num_steps, k, reward_distribution, **kwargs):
@@ -115,9 +123,6 @@ def run_bandits(num_steps, k, reward_distribution, **kwargs):
         action = policy.select_action()
         reward = reward_distribution.generate_rewards()[action]
         policy.update_action_value(action, reward)
-        # print("Reward means:", reward_distribution.means)
-        # print("Action value", policy.Q)
-        # print("\n")
         rewards[step] = reward
         optimal_actions[step] = (action == optimal_action)
         if kwargs["reward_dist"]["is_nonstationary"]:
